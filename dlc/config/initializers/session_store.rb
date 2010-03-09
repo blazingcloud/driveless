ade49a1b144294ab1d127430549eb61f82ee9595
Casey West <casey@geeknest.com>
2010-03-08 21:14:09 -0500

    Using mysql

--- dlc/config/initializers/session_store.rb
+++ dlc/config/initializers/session_store.rb
@@ -6,7 +6,7 @@
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_dlc_session',
-  :secret      => '5c2940ae46a7b7c9a3fdcda17aaf6252b820083de3f7e074b9d81c9a7cb29ed95d41e5d86c11710e1a6c96b415f91d69932990fbbb6ad3dcfb3b792f9fafc676'
+  :secret      => 'deb2364a6366ce982b6a77ddb996f179cdc2383e99aa926bc41d7c682af9bfaf1e1a5f3e9ddb84ba4da8a9985198fc93aa3e4446a234d16d12a9d02e3d364af4'
 }
 
 # Use the database for sessions instead of the cookie-based default,
