2c5dd167548861caa8231dbdb410b5d7be5f014d
Casey West <casey@geeknest.com>
2010-03-22 16:24:36 -0400

    sanitizing baseline

--- dlc/public/javascripts/application.js
+++ dlc/public/javascripts/application.js
@@ -1,2 +1,26 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+(function($) {
+  $(document).ready(function(){
+    $('.baseline-travel input').change(function(e){
+      var $baseline = $(this).parents('.baseline-travel:first');
+      var alone = parseFloat($('input:eq(0)', $baseline).val());
+      var green = parseFloat($('input:eq(1)', $baseline).val()) || 0;
+      if (alone) {
+        $('.number', $baseline).text(((green/alone)*100+'').substr(0,5));
+        $('.line-percent',$baseline).show();
+      } else {
+        $('.line-percent',$baseline).hide();
+      }
+      var $form  = $baseline.parents('.baseline');
+      var t_miles = _.reduce($('.baseline-travel input.alone', $form).get(), 0, function(t,i){return t+parseFloat($(i).val())});
+      var g_miles = _.reduce($('.baseline-travel input.green', $form).get(), 0, function(t,i){return t+(parseFloat($(i).val()) || 0)});
+      $('.total-miles', $form).text(t_miles);
+      $('.total-green', $form).text(g_miles);
+      if (t_miles) {
+        $('.foot .number', $form).text(((g_miles/t_miles)*100+'').substr(0,5));
+        $('.foot .line-percent', $form).show();
+      } else {
+        $('.foot .line-percent', $form).hide();
+      }
+    }).trigger('change');
+  });
+})(jQuery);
