8cca24d30320d271e56a23f9b848286efd09cfca
Casey West <casey@geeknest.com>
2010-04-01 14:11:45 -0400

    More style updates

--- dlc/app/helpers/application_helper.rb
+++ dlc/app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link_to_nav(name, options, html_options = {})
+    html_class = current_page?(options) ? 'current' : ''
+    "<li class='#{html_class}'>" + 
+      link_to(name, options, html_options) +
+    '</li>'
+  end
 end
