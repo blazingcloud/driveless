2d9e1b15dfe165fe5bdf522f8b5ea42ebe9818d9
Bruno Aguirre <yo@brunoaguirre.com>
2010-04-12 16:10:39 -0300

    Add the possibility for a user to be the admin

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100408232334) do
+ActiveRecord::Schema.define(:version => 20100412142230) do
 
   create_table "baselines", :force => true do |t|
     t.integer  "user_id"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(:version => 20100408232334) do
     t.string   "openid_identifier"
     t.string   "username"
     t.string   "pseudonym"
+    t.boolean  "admin",               :default => false
   end
 
 end
