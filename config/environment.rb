a6dbc0fbc7db1547dfcba6fb91345add36859b1b
Diego Algorta <diego@oboxodo.com>
2010-04-13 18:01:38 -0300

    Add dependency on http://googlecharts.rubyforge.org/

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem "will_paginate"
   config.gem "will_paginate"
   config.gem 'formtastic'
+  config.gem "googlecharts"
   # config.gem "compass-960-plugin"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
