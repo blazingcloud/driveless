e56dcff22c0360e356453b3e264dd4cd3ee2de8a
Bruno Aguirre <yo@brunoaguirre.com>
2010-04-13 13:40:37 -0300

    Fix some tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ describe User do
   it { should be_invalid }
   it { should have_one(:baseline) }
   it { should have_many(:trips) }
-  it { should have_many(:memberships) }
-  it { should have_many(:groups, :through => :memberships) }
+  it { should have_many(:memberships).dependent(:destroy) }
+  it { should have_many(:groups).through(:memberships) }
   it { should belong_to(:community) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:username) }
