6581354992348b6e4910ad851742376eb149b2c8
Bruno Aguirre <yo@brunoaguirre.com>
2010-04-13 11:18:52 -0300

    Basic Community blueprint

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -1,16 +1,10 @@
 require 'spec_helper'
 
 describe Community do
-  before(:each) do
-    @valid_attributes = {
-      :name => "value for name",
-      :state => "value for state",
-      :country => "value for country",
-      :description => "value for description"
-    }
+  before do
   end
 
   it "should create a new instance given valid attributes" do
-    Community.create!(@valid_attributes)
+    Community.make
   end
 end
--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -5,3 +5,17 @@ Spec::Runner.configure do |config|
   config.before(:all)  { Sham.reset(:before_all) }
   config.before(:each) { Sham.reset(:before_each) }
 end
+
+Sham.define do
+  city        { Faker::Address.city     }
+  state       { Faker::Address.us_state }
+  country     { Faker::Lorem.words(1)   }
+  description { Faker::Lorem.paragraph  }
+end
+
+Community.blueprint do
+  name        { Sham.city }
+  state
+  country
+  description
+end
