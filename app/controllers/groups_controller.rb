414aa6a66c3fb6e9f9b808a8b4ce892dd70e8040
Bruno Aguirre <yo@brunoaguirre.com>
2010-04-14 11:38:15 -0300

    Added CRUD For Groups to users
    * Added "My Groups" tab
    * Users can CRUD own groups
    * Users can list all the groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,52 @@
 class GroupsController < ApplicationController
-  def index
+
+  before_filter :collect_destinations, :only => [ :new, :edit ]
+  before_filter :is_the_owner?, :only => [ :destroy, :edit ]
+
+  def show
     @groups = Group.paginate(:page => params[:page] || 1)
+  end
+  def index
+    @groups = Group.paginate(:page => params[:page] || 1, :conditions => { :owner_id => current_user })
+  end
+  def new
+    @group = Group.new
+  end
+  def create
+    @group = Group.new(params[:group])
+    if @group.save
+      flash[:notice] = "Group #{@group.name} created successfully!"
+      redirect_to groups_url
+    else
+      flash[:alert] = "There Are Errors in the fields!"
+      redirect_to new_group_url
+    end
+  end
+  def edit
+    @group = current_group
+    render :new
+  end
+  def update
+    @group = current_group
+    redirect_to groups_url
+  end
+  def destroy
+    current_group.destroy
+    redirect_to groups_url
+  end
+
+  def current_group
+    Group.find(params[:id])
+  end
+
+  private
+
+  def is_the_owner?
+    group = Group.find(params[:id])
+    group.owner == current_user
+  end
+
+  def collect_destinations
+    @destinations = Destination.all
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,16 @@
 class Group < ActiveRecord::Base
   has_many :memberships, :dependent => :destroy
   has_many :users, :through => :memberships
+
   belongs_to :owner, :class_name => "User"
   belongs_to :destination
 
+  after_create :create_membership
+
   validates_presence_of :name, :owner_id, :destination_id
+
+  def create_membership
+    membership = Membership.new({:user_id => self.owner_id, :group_id => self.id})
+    membership.save
+  end
 end
