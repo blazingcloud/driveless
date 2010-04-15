44ee87a7c702fa373715a38f7797cc5850d60167
Bruno Aguirre and Diego Algorta <dev+elcuervo+oboxodo@cuboxsa.com>
2010-04-15 15:55:44 -0300

    Remove unneeded migration

--- db/migrate/20100415140608_add_is_green_column_to_modes.rb
+++
@@ -1,9 +0,0 @@
-class AddIsGreenColumnToModes < ActiveRecord::Migration
-  def self.up
-    add_column :modes, :is_green, :boolean, :default => true
-  end
-
-  def self.down
-    remove_column :modes, :is_green
-  end
-end
