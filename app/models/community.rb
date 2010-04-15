0f729d33b16168d8da66dc0d80e1bd2e58c33773
Bruno Aguirre and Diego Algorta <dev+elcuervo+oboxodo@cuboxsa.com>
2010-04-15 19:46:42 -0300

    Added "Show All Communities" in the "My Community" section
    * Added "greeniest" communities leader board
    * Added link from the "My Community" section
    * Added links in the "Communities" section to view users from that
      community leaderboard

--- app/models/community.rb
+++ app/models/community.rb
@@ -4,6 +4,13 @@ class Community < ActiveRecord::Base
 
   before_destroy :ensure_there_are_no_members
 
+  # Is there any 'rubiest' way to do this?
+  named_scope :by_green_miles,
+    :select => ['communities.*, SUM(users.green_miles) as green_miles'],
+    :joins => :users,
+    :group => [:"users.community_id"],
+    :order => ['green_miles DESC']
+
   private
 
   def ensure_there_are_no_members
