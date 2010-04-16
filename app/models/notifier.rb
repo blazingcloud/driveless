b44fd27d85db127a76ef46b5585e80fd6f6e5ffd
Bruno Aguirre <yo@brunoaguirre.com>
2010-04-16 18:17:58 -0300

    Added friend notification subject

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   default_url_options[:host] = "driveless.heroku.com"
 
   def password_reset_instructions(user)
-    subject       "Password Reset for Driveless"
+    subject       "Password Reset for Driveless <noreply@driveless.heroku.com>"
     from          "Driveless Challenge"
     recipients    user.email
     sent_on       Time.now
@@ -10,8 +10,8 @@ class Notifier < ActionMailer::Base
   end
 
   def friendship_notification(user, friend)
-    subject       ""
-    from          "Driveless Challange"
+    subject       "#{user.username} added you as a friend"
+    from          "Driveless Challange <noreply@driveless.heroku.com>"
     recipients    friend.email
     sent_on       Time.now
     body          :friend_username => user.username, :friend_url => user_url( user )
