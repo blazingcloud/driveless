9173166adc86c858fa479101dcc862d2e509ede4
Casey West <casey@geeknest.com>
2010-04-16 18:18:14 -0400

    prettyness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def link_to_nav(name, options, html_options = {})
+    description = html_options[:description] ? html_options.delete(:description) : ''
     html_class = current_page?(options) ? 'current' : ''
     "<li class='#{html_class}'>" + 
       link_to(name, options, html_options) +
+      "<span class='desc'>#{description}</span>" +
     '</li>'
   end
 
