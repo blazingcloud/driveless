daeb83e05789c4da82b13954e0d7e8c53786ed2e
Casey West <casey@geeknest.com>
2010-04-17 14:51:01 -0400

    Adding new mode names and co2s

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,22 +11,24 @@ Unit.create([
 ])
 
 =begin
-Solo Car Trips (averaging 24 mpg)         0.843
-Bus (VTA)         0.240
-Train (Caltrain, BART)         0.370
-Carpool (2 people)         0.422
-Hybrid / Electric Car (averaging 50 mpg)         0.405
+Mode Name in Application	Mode Description	Pounds of CO2 Avoided per Mile
+Bike	Bike	0.843
+Walk	Walk (includes skateboarad, rollerblade, etc)	0.843
+Bus	Bus (e.g. VTA)	0.603
+Train	Train (e.g. Caltrain, BART)	0.473
+Carpool	Carpool (2 people or more)	0.422
+Drove Car Alone	Solo Car Trips (assumes 24 mpg average, includes all categories of car, hybrid or electric, SUV, pickups )	0.000
 =end
 
 Mode.create([
-  {:name => 'Walk',                             :green => true,   :lb_co2_per_mile => 0.840},
-  {:name => 'Bike',                             :green => true,   :lb_co2_per_mile => 0.840},
+  {:name => 'Walk (includes skateboarad, rollerblade, etc)', :green => true,   :lb_co2_per_mile => 0.843},
+  {:name => 'Bike',                             :green => true,   :lb_co2_per_mile => 0.843},
   {:name => 'Solo Car Trips (avg 24mpg)',       :green => true,   :lb_co2_per_mile => 0.843},
-  {:name => 'Bus (VTA)',                        :green => true,   :lb_co2_per_mile => 0.240},
-  {:name => 'Train (Caltrain,BART)',            :green => true,   :lb_co2_per_mile => 0.370},
+  {:name => 'Bus (VTA)',                        :green => true,   :lb_co2_per_mile => 0.603},
+  {:name => 'Train (Caltrain,BART)',            :green => true,   :lb_co2_per_mile => 0.473},
   {:name => 'Carpool (2 people)',               :green => true,   :lb_co2_per_mile => 0.422},
-  {:name => 'Hybrid/Electric Car (avg 50mpg)',  :green => true,   :lb_co2_per_mile => 0.405},
-  {:name => 'Drive Alone',                      :green => false,  :lb_co2_per_mile => 0.000}
+#  {:name => 'Hybrid/Electric Car (avg 50mpg)',  :green => true,   :lb_co2_per_mile => 0.405},
+  {:name => 'Solo Car Trips (all cars)',        :green => false,  :lb_co2_per_mile => 0.000}
 ])
 
 Destination.create([
