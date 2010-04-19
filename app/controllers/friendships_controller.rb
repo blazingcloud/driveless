d90124db76bf2de0b811d4ff995e1e6909a7df1b
Bruno Aguirre <yo@brunoaguirre.com>
2010-04-19 18:07:57 -0300

    Centralized leaderboard in shared/leaderboard
    * Added one and only template for leaderboards

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,13 +3,13 @@ class FriendshipsController < ApplicationController
   before_filter :require_user
 
   def index
-    @friends = current_user.friends.by_green_miles
-    @fans = User.find(Friendship.of(current_user).map{|f| f.user_id})
+    @friends = current_user.friends.by_green_miles.paginate(:page => params[:page] || 1)
+    @fans = User.find(Friendship.of(current_user).map{|f| f.user_id}).paginate(:page => params[:page] || 1)
   end
 
   def show
-    @friends = User.find_by_name(params[:username]).friends.by_green_miles
-    @fans = User.find( Friendship.of( User.find_by_name(params[:username]) ).map{|f| f.user_id} )
+    @friends = User.find_by_name(params[:username]).friends.by_green_miles.paginate(:page => params[:page] || 1)
+    @fans = User.find( Friendship.of( User.find_by_name(params[:username]) ).map{|f| f.user_id} ).paginate(:page => params[:page] || 1)
   end
 
   def destroy
