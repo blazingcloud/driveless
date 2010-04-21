0ff1db75814926f6c2bbe7930e4b9f5f053c774d
Casey West <casey@geeknest.com>
2010-04-21 15:04:44 -0400

    Adjust wording and default host for invites and other notifications

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,9 +1,10 @@
 class Notifier < ActionMailer::Base
-  default_url_options[:host] = "driveless.heroku.com"
+  @host = "my.drivelesschallenge.com"
+  default_url_options[:host] = @host
 
   def password_reset_instructions(user)
     subject       "Password Reset for Driveless "
-    from          "Driveless Challenge <noreply@driveless.heroku.com>"
+    from          "Driveless Challenge <noreply@#{@host}>"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
@@ -11,7 +12,7 @@ class Notifier < ActionMailer::Base
 
   def friendship_notification(user, friend)
     subject       "#{user.username} added you as a friend"
-    from          "Driveless Challange <noreply@driveless.heroku.com>"
+    from          "Driveless Challange <noreply@#{@host}>"
     recipients    friend.email
     sent_on       Time.now
     body          :friend_username => user.username, :friend_url => user_url( user )
@@ -19,7 +20,7 @@ class Notifier < ActionMailer::Base
 
   def join_invitation( user, invitation )
     subject       "You're invited to Drive Less by #{user.username}"
-    from          "Driveless Challange <noreply@driveless.heroku.com>"
+    from          "Driveless Challange <noreply@#{@host}>"
     recipients    invitation[:email]
     sent_on       Time.now
     body          :invited => invitation[:name], :user => user.username, :body => invitation[:invitation]
