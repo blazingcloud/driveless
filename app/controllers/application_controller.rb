52146f93d350768a0a360fd87916424923d93f0a
Jen-Mei Wu <jenmei@colorxp.com>
2011-04-14 06:55:11 -0700

    WIP: Created a new Rails 3 project. Copied our files into it, reviewed differences. Not finished ... this is being saved just an interim commit of files we're pretty sure need to be changed. Specs do not pass or even run.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery
   require 'authlogic'
 
   helper :all # include all helpers, all the time
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Driveless::Application.config.secret_token = '6386ab2f98fde047f2064d6729a7060f2a3db15239244eaacfedb29a901ba9b8affc450402aba486c9e506541b6b5dacf9384d1f147f8ac0eca97933f2298c15'
