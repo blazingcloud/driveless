2dac285bd5af98afd9cc93feeb07b91015d543ca
Jen-Mei Wu <jenmei@colorxp.com>
2011-04-21 15:36:25 -0700

    Remove confirmable feature (may add in the future per Adina). Added redirects for old routes. Front page redirects to user's page if logged in; otherwise redirects to login page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :token_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :token_authenticatable, :registerable, #:confirmable,
          :recoverable, :rememberable, :trackable, 
          :validatable, :encryptable #, :omniauthable
 
