e1cffc16bad80f20a47febcf1b4ddcff59bd3897
Jen-Mei Wu <pair+jen-mei@blazingcloud.net>
2011-04-22 01:45:50 -0700

    Delete button works on for trips but should look a bit better. User's page shows up on /accounts.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      redirect_to current_user
+      redirect_to account_path
     else
       redirect_to new_user_session_path
     end
