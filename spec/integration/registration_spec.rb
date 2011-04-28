45d6d3a07eee269f9eaf21d5431abc3eb597ee82
Jen-Mei Wu <pair+jen-mei@blazingcloud.net>
2011-04-28 11:37:14 -0700

    All specs pass.

--- spec/integration/registration_spec.rb
+++ spec/integration/registration_spec.rb
@@ -1,94 +1,93 @@
 require 'spec_helper'
 
 describe "Registration" do
+
+  def register(args={})
+    fill_in "user[username]", :with => "Moogles"
+    fill_in "user[email]", :with => "moogles@example.com"
+    fill_in "user[name]", :with => "Moog"
+    fill_in "user[address]", :with => "930 Cement St"
+    fill_in "user[city]", :with => "New City"
+    fill_in "user[zip]", :with => "94102"
+    check "user[is_13]"
+    check "user[is_parent]" if args[:is_parent] == true
+    check "user[read_privacy]"
+    fill_in "user[password]", :with => "password"
+    fill_in "user[password_confirmation]", :with => "password"
+    click_button "Register"
+    @user = User.find_by_email('moogles@example.com')
+  end
+
   before do
     visit('/')
   end
+
   context "when not signed up" do
     it "should have sign up button" do
       page.should have_content("Signup now!")
     end
-    
+
     context "clicking 'Signup now!'" do
       before do
         click_link("Signup now!")
         current_path.should == '/users/sign_up'
       end
-      
+
       context "filling out form with valid attributes" do
         before do
           register
         end
-        
+
         it "should create a new user" do
           @user.should be_present
           @user.should_not be_is_parent
         end
-        
+
         it "should take us to the right page" do
           current_path.should == account_path
-          #save_and_open_page
-          page.should have_content("Registration successful.")
         end
-        
+
         context "logging in" do
           before do
             click_link("Logout")
-            page.should have_content("Logout successful!")
-            current_path.should == '/'
+            current_path.should == '/users/sign_in'
           end
-          
+
           it "with valid username and password" do
-            fill_in "user_session[username]", :with => "Moogles"
-            fill_in "user_session[password]", :with => "password"
+            fill_in "Email", :with => "moogles@example.com"
+            fill_in "Password", :with => "password"
             click_button "Login"
-          #save_and_open_page
-            page.should have_content("Successfully logged in.")
+            page.should have_content("Log a new trip")
           end
         end
       end
-      
+
       context "filling out form with valid attributes as parent" do
         before do
           register(:is_parent => true)
         end
-        
+
         it "should create a new user who is a parent" do
           @user.should be_present
           @user.should be_is_parent
         end
-        
+
         it "should take us to the right page" do
           current_path.should == account_path
-          page.should have_content("Registration successful.")
+          page.should have_content("Log a new trip")
         end
       end
     end
   end
-  
+
   context "when signed up, logging in" do
     it "should have Login button" do
       page.should have_content("Login")
     end
-    
+
     it "should have 'Forgot Your Password?' button" do
-      page.should have_content("Forgot your nickname or password?")
+      page.should have_content("Forgot your password?")
     end
   end
 end
 
-def register(args={})
-  fill_in "user[username]", :with => "Moogles"
-  fill_in "user[email]", :with => "moogles@example.com"
-  fill_in "user[name]", :with => "Moog"
-  fill_in "user[address]", :with => "930 Cement St"
-  fill_in "user[city]", :with => "New City"
-  fill_in "user[zip]", :with => "94102"
-  check "user[is_13]"
-  check "user[is_parent]" if args[:is_parent] == true
-  check "user[read_privacy]"
-  fill_in "user[password]", :with => "password"
-  fill_in "user[password_confirmation]", :with => "password"
-  click_button "Register"
-  @user = User.find_by_email('moogles@example.com')
-end
