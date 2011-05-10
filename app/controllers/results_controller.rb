32c6bf6b189cb109cf6508651cf34816edc479fe
Jen-Mei Wu <pair+jen-mei@blazingcloud.net>
2011-05-09 19:47:54 -0700

    Changed results to display tabular data in tables.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,10 +2,22 @@ class ResultsController < ApplicationController
   before_filter :require_admin
 
   def index
-    @green_mode_results = Mode.green.map do |mode|
-      result = User.max_miles(mode.name)
-    end
-    @most_green_trips_result = User.max_green_trips
-    @most_green_shopping_trips_result = User.max_green_shopping_trips
+    @categories = [
+      {
+        :description => "Most Green Miles for All Users by Mode of Transportation",
+        :results => Mode.green.map { |mode| result = User.max_miles(mode.name) }
+      },
+
+      {
+        :description => "Most Green Trips for All Users",
+        :results => [User.max_green_trips]
+      },
+
+      {
+        :description => "Most Green Shopping Trips for All Users",
+        :results => [User.max_green_shopping_trips]
+      },
+    ]
+
   end
 end
