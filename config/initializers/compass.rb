b02ecaba9a453fb8b7f321851b6fc85fe59cdef8
Curtis Schofield <curtis@ram9.cc>
2012-03-26 14:19:06 -0700

    trying to fix heroku tmp dir issue

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,13 @@
+
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+                                                 :urls => ['/stylesheets'],
+                                                     :root => "#{Rails.root}/tmp")
 require 'compass'
 # If you have any compass plugins, require them here.
 Compass.add_project_configuration(File.join(Rails.root, "config", "compass.config"))
 Compass.configuration.environment = Rails.env.to_sym
 Compass.configure_sass_plugin!
+
