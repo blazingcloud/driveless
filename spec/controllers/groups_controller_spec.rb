37d0ed5b2106a923d2da63cbbcbe7460de0dfc57
Curtis Schofield <curtis@ram9.cc>
2012-03-26 15:58:42 -0700

    spec for modifying group

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,25 +2,45 @@ require 'spec_helper'
 
 describe GroupsController do
   include Devise::TestHelpers
-  context "when deleting a group" do
+  context "with an owner and a group" do
     before do
       @owner = User.make
       @destination = Destination.make(:name => 'Madison WI')
       @group = Group.make(:name => 'My Group', :owner => @owner, :destination => @destination)
     end
-
-    it "cannot be destroyed by someone other than the owner" do
-      lambda do
-        delete :destroy, :id => @group.id
-      end.should_not change(Group,:count).by(-1)
-
+    context "when editing a group " do
+      it "cannot be edited by someone other than the owner" do
+        @group.name.should_not == 'bean'
+        put :update, :id => @group.id, :group => {:name => 'bean'}
+        @group.reload.name.should_not == 'bean'
+      end
+      context "as the owner" do
+        before do
+          sign_in @owner
+        end
+        it "can be edited" do
+          @group.name.should_not == 'bean'
+          put :update, :id => @group.id, :group => {:name => 'bean'}
+          @group.reload.name.should == 'bean'
+        end
+      end
     end
-    context "as the owner" do
-      it "can be destroyed" do
-        sign_in @owner
+    context "when deleting a group" do
+      it "cannot be destroyed by someone other than the owner" do
         lambda do
           delete :destroy, :id => @group.id
-        end.should change(Group,:count).by(-1)
+        end.should_not change(Group,:count).by(-1)
+
+      end
+      context "as the owner" do
+        before do
+          sign_in @owner
+        end
+        it "can be destroyed" do
+          lambda do
+            delete :destroy, :id => @group.id
+          end.should change(Group,:count).by(-1)
+        end
       end
     end
   end
