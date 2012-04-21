f4c39ec49e43c31a78e8b25e4838e517d4f85f4c
Curtis Schofield <curtis@ram9.cc>
2012-04-20 17:28:42 -0700

    footer has faq

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120419205111) do
+ActiveRecord::Schema.define(:version => 20120420235006) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
